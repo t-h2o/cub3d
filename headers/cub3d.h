31c6948aac8f3966d46d59da86aa0c9715b69163
To grivel <grivel.theo@protonmail.com>
2022-08-17 21:31:42 +0200

    cub3d.h: Added protection with define

--- headers/cub3d.h
+++ headers/cub3d.h
@@ -6,8 +6,13 @@
 /*   By: gudias <marvin@42lausanne.ch>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 18:51:07 by gudias            #+#    #+#             */
-/*   Updated: 2022/08/17 18:55:50 by gudias           ###   ########.fr       */
+/*   Updated: 2022/08/17 21:30:00 by melogr@phy       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include	"stdio.h"
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include	"stdio.h"
+
+#endif /* CUB3D_H */
