c88db81fced4317df33bab9775f6e8e6e7105ef2
To grivel <grivel.theo@protonmail.com>
2022-08-17 21:34:03 +0200

    cub3d.h: Created s_info

--- headers/cub3d.h
+++ headers/cub3d.h
@@ -6,13 +6,22 @@
 /*   By: gudias <marvin@42lausanne.ch>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 18:51:07 by gudias            #+#    #+#             */
-/*   Updated: 2022/08/17 21:30:00 by melogr@phy       ###   ########.fr       */
+/*   Updated: 2022/08/17 21:37:45 by melogr@phy       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
+// HEADERS
+
 # include	"stdio.h"
+
+// STRUCTURE
+
+// map: We save the map in the table map
+typedef struct s_info {
+	char	**map;
+}	t_info;
 
 #endif /* CUB3D_H */
