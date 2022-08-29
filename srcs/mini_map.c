23a678db6abe79da2b1e0c08a29f7678d76ed22f
Theo Grivel <tgrivel@c3r5s5.42lausanne.ch>
2022-08-29 16:56:31 +0200

    Print player
    Signed-off-by: Theo Grivel <tgrivel@c3r5s5.42lausanne.ch>

--- srcs/mini_map.c
+++ srcs/mini_map.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@42lausanne.ch>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/20 12:16:47 by user42            #+#    #+#             */
-/*   Updated: 2022/08/27 17:40:45 by gudias           ###   ########.fr       */
+/*   Updated: 2022/08/29 14:41:45 by tgrivel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,9 @@
 // Position:
 //   x: Size * x_char + distance from the left
 //   y: Size * y_char + distance from the top
+// Get player position:
+//   x: Size * x_char + ((SQUARE_MAP - SQUARE_PLAYER) / 2)
+//   y: Size * y_char + ((SQUARE_MAP - SQUARE_PLAYER) / 2)
 static void	print_square(t_info *info, char c, int y, int x)
 {
 	if (c == '0' || c == 'N' || c == 'E' || c == 'S' || c == 'W')
@@ -28,10 +31,30 @@ static void	print_square(t_info *info, char c, int y, int x)
 			info->mm_img[WALL],
 			x * SS_MAP + MM_POS_X,
 			y * SS_MAP + MM_POS_Y);
+	if (c == 'N' || c == 'E' || c == 'S' || c == 'W')
+	{
+		if (info->pos_player[X] == -1.0f)
+		{
+			info->pos_player[X]
+				= (float)(x * SS_MAP) + ((SS_MAP - SS_PLAYER) / 2);
+			info->pos_player[Y]
+				= (float)(y * SS_MAP) + ((SS_MAP - SS_PLAYER) / 2);
+			info->pos_player[A] = 0.0f;
+		}
+	}
+}
+
+static void	print_player(t_info *info)
+{
+	mlx_put_image_to_window(info->mlx[0], info->mlx[1],
+		info->mm_img[PLAYER],
+		MM_POS_X + info->pos_player[X],
+		MM_POS_Y + info->pos_player[Y]);
 }
 
 // Clear the window
 // Print the mini map
+// Print the player
 void	print_minimap(t_info *info)
 {
 	int		pos_x;
@@ -46,4 +69,5 @@ void	print_minimap(t_info *info)
 		while (info->map[pos_y][++pos_x])
 			print_square(info, info->map[pos_y][pos_x], pos_y, pos_x);
 	}
+	print_player(info);
 }
