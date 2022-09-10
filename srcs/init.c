7608a2aabcee6094cd83d65f0e55ede406661c56
To grivel <grivel.theo@protonmail.com>
2022-09-10 23:01:50 +0200

    set to 0 info struct

--- srcs/init.c
+++ srcs/init.c
@@ -6,7 +6,7 @@
 /*   By: gudias <marvin@42lausanne.ch>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/20 17:44:17 by gudias            #+#    #+#             */
-/*   Updated: 2022/09/10 20:49:37 by melogr@phy       ###   ########.fr       */
+/*   Updated: 2022/09/10 23:01:16 by melogr@phy       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,13 @@
 // Load textures
 int	init_game(t_info *info, char *mapname)
 {
-	info->map = NULL;
-	info->map_h = 0;
+	ft_memset(info, 0, (sizeof(t_info)));
 	info->active_map = true;
 	info->player.x = -1.0f;
 	info->player.dx = -1.0f;
 	info->player.dy = -1.0f;
-	ft_memset(&(info->texture), 0, (sizeof(t_texture)) * 6);
 	info->mlx[INIT] = mlx_init();
 	info->mlx[WINDOW] = 0;
-	ft_memset(&(info->mm_img), 0, (sizeof(void *) * 3));
 	if (!info->mlx[INIT])
 		return (error_msg("Couldn't init mlx"));
 	if (load_map(info, mapname) || load_textures(info))
