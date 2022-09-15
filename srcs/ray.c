3488ac73564cb052204e61637de472ea1e244984
Theo Grivel <tgrivel@c3r4s2.42lausanne.ch>
2022-09-15 10:12:24 +0200

    Updated ray()
    Added float hit[2] as argument instead float ray[2]

--- srcs/ray.c
+++ srcs/ray.c
@@ -6,31 +6,31 @@
 /*   By: tgrivel <marvin@42lausanne.ch>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/24 17:04:24 by tgrivel           #+#    #+#             */
-/*   Updated: 2022/09/15 10:43:20 by tgrivel          ###   ########.fr       */
+/*   Updated: 2022/09/15 12:19:16 by tgrivel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"cub3d.h"
 
 // ray[X, Y]: The position of the ray
-static void	ray(t_info *info, float delta[2])
+static void	ray(t_info *info, float delta[2], float hit[2])
 {
-	float	ray[2];
-
-	ray[X] = info->player.pos[X];
-	ray[Y] = info->player.pos[Y];
-	while (info->map[(int)(ray[Y])][(int)(ray[X])] != '1')
+	hit[X] = info->player.pos[X];
+	hit[Y] = info->player.pos[Y];
+	while (info->map[(int)(hit[Y])][(int)(hit[X])] != '1')
 	{
 		mlx_pixel_put(info->mlx[INIT], info->mlx[WINDOW],
-			ray[X] * MM_SIZE_TILE + MM_POS_X,
-			ray[Y] * MM_SIZE_TILE + MM_POS_Y,
+			hit[X] * MM_SIZE_TILE + MM_POS_X,
+			hit[Y] * MM_SIZE_TILE + MM_POS_Y,
 			CO_BLUE);
-		ray[X] += delta[X] / MM_SIZE_TILE;
-		ray[Y] += delta[Y] / MM_SIZE_TILE;
+		hit[X] += delta[X] / MM_SIZE_TILE;
+		hit[Y] += delta[Y] / MM_SIZE_TILE;
 	}
 }
 
 void	player_ray(t_info *info)
 {
-	ray(info, info->player.delta);
+	float	hit[2];
+
+	ray(info, info->player.delta, hit);
 }
