85d8835a452ee494c755271c36b099e051a8ba01
Theo Grivel <tgrivel@c1r11s8.42lausanne.ch>
2022-09-28 23:29:44 +0200

    Changed init order
    If the map was invalid, mlx_init() was call.
    Better way to check the map before to call mlx_init()
    0. Init default value
    1. Read and check the map
    2. mlx_init()
    3. Load texture

--- srcs/init.c
+++ srcs/init.c
@@ -34,11 +34,13 @@ int	init_game(t_info *info, char *mapname)
 	info->player.pos[X] = -1.0f;
 	info->player.delta[X] = -1.0f;
 	info->player.delta[Y] = -1.0f;
-	info->mlx[INIT] = mlx_init();
 	init_ray(info);
+	if (load_map(info, mapname))
+		return (1);
+	info->mlx[INIT] = mlx_init();
 	if (!info->mlx[INIT])
 		return (error_msg("Couldn't init mlx"));
-	if (load_map(info, mapname) || load_textures(info))
+	if (load_textures(info))
 		return (1);
 	return (0);
 }
