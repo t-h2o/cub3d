bb4984a890f5e54978d109c801fcf9e5590ea1a7
gudias <gudias@42.ch>
2022-09-30 15:38:10 +0200

    cleaned main.c

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: gudias <marvin@42lausanne.ch>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 18:47:21 by gudias            #+#    #+#             */
-/*   Updated: 2022/09/10 20:30:17 by gudias           ###   ########.fr       */
+/*   Updated: 2022/09/30 15:37:56 by gudias           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,27 +17,10 @@ int	main(int ac, char **av)
 	t_info	info;
 	int		i;
 
-	ft_putstr("Cub3D\n");
-
 	if (ac != 2)
 		exit(error_msg("Invalid argument"));
-
 	if (init_game(&info, av[1]))
-		close_game(&info, 1);
-	
-	//print map informations
-	printf("NO: %s\nSO: %s\nEA: %s\nWE: %s\nF: %s\nC: %s\n", info.texture[NO].path, info.texture[SO].path, info.texture[EA].path, info.texture[WE].path, info.texture[FL].path, info.texture[CE].path);
-	
-	//print the map	
-	if (info.map)
-	{
-		i = 0;
-		while (info.map[i])
-		{
-			ft_printf("%s", info.map[i]);
-			i++;
-		}
-	}
+		close_game(&info, 1);	
 	if (start_window(&info))
 		close_game(&info, 1);
 	return (0);
