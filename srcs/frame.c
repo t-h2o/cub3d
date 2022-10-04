cefd9e0252c2a9ac0bede85b124a67a30279536c
To grivel <grivel.theo@protonmail.com>
2022-10-04 10:04:25 +0200

    Removed print_info()

--- srcs/frame.c
+++ srcs/frame.c
@@ -49,25 +49,6 @@ static void	player_ray(t_info *info)
 	anti_bad(info);
 }
 
-// Print infos: player position (x, y, angle)
-static void	print_info(t_info *info)
-{
-	char	message[255];
-
-	ft_memset(message, 0, 255);
-	sprintf(message, "Position X: %f", info->player.pos[X]);
-	mlx_string_put(info->mlx[INIT], info->mlx[WINDOW],
-		500, 10, CO_WHITE, message);
-	ft_memset(message, 0, 255);
-	sprintf(message, "Position Y: %f", info->player.pos[Y]);
-	mlx_string_put(info->mlx[INIT], info->mlx[WINDOW],
-		500, 20, CO_WHITE, message);
-	ft_memset(message, 0, 255);
-	sprintf(message, "P. angle  : %f", info->player.angle);
-	mlx_string_put(info->mlx[INIT], info->mlx[WINDOW],
-		500, 30, CO_WHITE, message);
-}
-
 // 1. Cast rays to get view informations
 // 2. Render the view screen
 // 3. Print the screen
@@ -81,5 +62,4 @@ void	print_frame(t_info *info)
 	mlx_put_image_to_window(info->mlx[0], info->mlx[1], info->screen.img, 0, 0);
 	if (info->active_map)
 		print_minimap(info);
-	print_info(info);
 }
