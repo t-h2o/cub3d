2a5d630680da5e0a41008997c60dc87c52349325
gudias <gudias@42.ch>
2022-10-10 17:35:32 +0200

    new approach: removed mouse_hook -> good result

--- srcs/window.c
+++ srcs/window.c
@@ -6,7 +6,7 @@
 /*   By: melogr@phy <tgrivel@student.42lausanne.ch  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 22:01:59 by melogr@phy        #+#    #+#             */
-/*   Updated: 2022/10/10 16:45:57 by gudias           ###   ########.fr       */
+/*   Updated: 2022/10/10 17:32:40 by gudias           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,6 +59,22 @@ static int	red_cross(t_info *info)
 	return (0);
 }
 
+// Mouse movement
+// get position of the mouse
+// rotation = mouse_X_offset * RAD_PER_PIXEL
+// reset position to the middle of the screen
+static void	mouse_move(t_info *info)
+{
+	int	x;
+	int	y;
+
+	mlx_mouse_get_pos(info->mlx[WINDOW], &x, &y);
+	if (x == W_WIDTH / 2)
+		return ;	
+	player_rotate(info, (x - (W_WIDTH / 2)) * (FOV / W_WIDTH));
+	my_mouse_move(info->mlx[0], info->mlx[1], W_WIDTH / 2, W_HEIGHT / 2);
+}
+
 // Loop hook
 // Update frame
 static int	update_frame(t_info *info)
@@ -75,22 +91,8 @@ static int	update_frame(t_info *info)
 		player_rotate(info, PS_ROTATE);
 	else if (info->inputs.r_left)
 		player_rotate(info, -PS_ROTATE);
+	mouse_move(info);
 	print_frame(info);
-	return (0);
-}
-
-// Mouse hook
-static int	mouse_move(int x, int y, t_info *info)
-{
-	static int	last_x = W_WIDTH / 2;
-
-	(void) info;
-	(void) y;
-	if (x < last_x)
-		player_rotate(info, -PS_ROTATE * PS_MOUSE);
-	else if (x > last_x)
-		player_rotate(info, PS_ROTATE * PS_MOUSE);
-	my_mouse_move(info->mlx[0], info->mlx[1], W_WIDTH / 2, W_HEIGHT / 2);
 	return (0);
 }
 
@@ -105,10 +107,10 @@ int	start_window(t_info *info)
 		return (error_msg("Couldn't create window"));
 	mlx_hook(info->mlx[WINDOW], 2, 1L << 0, deal_key, info);
 	mlx_hook(info->mlx[WINDOW], 3, 1L << 1, release_key, info);
-	mlx_hook(info->mlx[WINDOW], 6, 1L << 6, mouse_move, info);
 	mlx_hook(info->mlx[WINDOW], 17, 0L << 0, red_cross, info);
 	mlx_loop_hook(info->mlx[INIT], update_frame, info);
 	mlx_mouse_hide(info->mlx[INIT], info->mlx[WINDOW]);
+	my_mouse_move(info->mlx[0], info->mlx[1], W_WIDTH / 2, W_HEIGHT / 2);
 	print_frame(info);
 	mlx_loop(info->mlx[INIT]);
 	return (0);
