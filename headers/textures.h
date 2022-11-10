3b876eb35e6ed138074c5ec5ba88f5bee6cf0888
gudias <gudias@42.ch>
2022-11-10 20:41:47 +0100

    tx

--- headers/textures.h
+++ headers/textures.h
@@ -6,7 +6,7 @@
 /*   By: gudias <marvin@42lausanne.ch>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/30 11:58:14 by gudias            #+#    #+#             */
-/*   Updated: 2022/11/08 20:08:21 by gudias           ###   ########.fr       */
+/*   Updated: 2022/11/10 20:04:24 by gudias           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define TEXTURES_H
 
 // minimap textures
-# define MM_PLAYER	"assets/textures/minimap/player.xpm"
+# define MM_PLAYER	"assets/textures/mm/player.xpm"
 # define MM_EMPTY	0x70303030
 # define MM_GROUND	0x00C1C1C1
 # define MM_WALL	0x00000000
@@ -29,8 +29,8 @@
 # define TX_EAST	"assets/textures/wolftx/purplestone.xpm"
 # define TX_WEST	"assets/textures/wolftx/greystone.xpm"
 # define TX_FLOOR	"assets/textures/dirt1.xpm"
-# define TX_CEIL	"assets/textures/sky_doubled.xpm"
-# define TX_DOOR	"assets/textures/door_metal.xpm"
+# define TX_CEIL	"assets/textures/sky/sky_doubled.xpm"
+# define TX_DOOR	"assets/textures/doors/door_metal.xpm"
 
 // sprites
 # define TX_TORCH_NB	3
@@ -45,11 +45,11 @@
 // POV (hands, weapons, ...)
 # define TX_PISTOL_NB	6
 
-# define TX_PISTOL1	"assets/textures/pistol1/01.xpm"
-# define TX_PISTOL2	"assets/textures/pistol1/02.xpm"
-# define TX_PISTOL3	"assets/textures/pistol1/03.xpm"
-# define TX_PISTOL4	"assets/textures/pistol1/04.xpm"
-# define TX_PISTOL5	"assets/textures/pistol1/05.xpm"
-# define TX_PISTOL6	"assets/textures/pistol1/06.xpm"
+# define TX_PISTOL1	"assets/textures/pov/pistol1/01.xpm"
+# define TX_PISTOL2	"assets/textures/pov/pistol1/02.xpm"
+# define TX_PISTOL3	"assets/textures/pov/pistol1/03.xpm"
+# define TX_PISTOL4	"assets/textures/pov/pistol1/04.xpm"
+# define TX_PISTOL5	"assets/textures/pov/pistol1/05.xpm"
+# define TX_PISTOL6	"assets/textures/pov/pistol1/06.xpm"
 
 #endif /* TEXTURES_H */
